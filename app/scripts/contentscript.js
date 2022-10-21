0b80c0c02806d724e2e651a682ad833218ae085f
Jyoti Puri <jyotipuri@gmail.com>
2022-10-21 11:30:42 +0200

    Improvement in DAPP action replay logic.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -261,10 +261,12 @@ const setupPageStreams = () => {
   pageChannel = pageMux.createStream(PROVIDER);
 };
 
+let REPLAY_ENABLED = false;
 const setupExtensionStreams = () => {
+  REPLAY_ENABLED = true;
   extensionPort = browser.runtime.connect({ name: CONTENT_SCRIPT });
-  // extensionPort.onMessage.addListener(extensionStreamMessageListener);
   extensionStream = new PortStream(extensionPort);
+  extensionStream.on('data', extensionStreamMessageListener);
 
   // create and connect channel muxers
   // so we can handle the channels individually
@@ -464,25 +466,30 @@ function logStreamDisconnectWarning(remoteLabel, error) {
  * The function send message to inpage to notify it of extension stream connection
  * This is used as notification to replay any pending messages in MV3
  */
-// function extensionStreamMessageListener(msg) {
-//   if (isManifestV3 && msg.name === 'CONNECTION_READY') {
-//     window.postMessage(
-//       {
-//         target: INPAGE, // the post-message-stream "target"
-//         data: {
-//           // this object gets passed to obj-multiplex
-//           name: PROVIDER, // the obj-multiplex channel name
-//           data: {
-//             jsonrpc: '2.0',
-//             method: 'METAMASK_EXTENSION_STREAM_CONNECT',
-//           },
-//         },
-//       },
-//       window.location.origin,
-//     );
-//     extensionPort.onMessage.removeListener(extensionStreamMessageListener);
-//   }
-// }
+function extensionStreamMessageListener(msg) {
+  if (
+    REPLAY_ENABLED &&
+    isManifestV3 &&
+    (msg.data.method === 'metamask_unlockStateChanged' ||
+      msg.data.method === 'metamask_chainChanged')
+  ) {
+    REPLAY_ENABLED = false;
+    window.postMessage(
+      {
+        target: INPAGE, // the post-message-stream "target"
+        data: {
+          // this object gets passed to obj-multiplex
+          name: PROVIDER, // the obj-multiplex channel name
+          data: {
+            jsonrpc: '2.0',
+            method: 'METAMASK_EXTENSION_STREAM_CONNECT',
+          },
+        },
+      },
+      window.location.origin,
+    );
+  }
+}
 
 /**
  * This function must ONLY be called in pump destruction/close callbacks.
